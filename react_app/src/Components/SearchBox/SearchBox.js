5290d3ef04199d5b7c63bc8d4e0dbd7d49aee760
xheartean <info@gokhankoc.pl>
2020-07-09 23:05:58 +0200

    color palette added

---
+++ react_app/src/Components/SearchBox/SearchBox.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SearchBox.css";
+
+const SearchBox = () => {
+  return <div styleName="wrapper">Hello World Im SearchBox</div>;
+};
+
+export default SearchBox;
